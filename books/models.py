699653bedafc163ca16c4a3305e8146c14e31780
Stout <stoutd@pool-1-151.staff.net.dc.usn.root.ent>
2017-12-19 16:23:31 -0500

    Add Author object

--- books/models.py
+++ books/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class Book(models.Model):
 	title = models.CharField(max_length=150)
-	author = models.CharField(max_length=90, help_text="Use pen name, not real name")
+	authors = models.ManyToManyField("Author", related_name="books")
 	review = models.TextField(blank=True, null=True)
 	date_reviewed = models.DateTimeField(blank=True, null=True)
 	is_favorite = models.BooleanField(default=False, verbose_name="Favorite?")
 
 	def __str__(self):
 		return self.title
+
+class Author(models.Model):
+	name = models.CharField(max_length=70, help_text="Use pen name, not real name", unique=True)
+
+	def __str__(self):
+		return self.name
