06f636ab9a8281e74c64867b1b45e1533e70dfe6
Stout <stoutd@pool-1-151.staff.net.dc.usn.root.ent>
2017-12-18 19:58:15 -0500

    Update book model and field names, create superuser and add a couple book objects

--- books/models.py
+++ books/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Book(models.Model):
 	title = models.CharField(max_length=150)
-	author= models.CharField(max_length=70)
- 	review = models.TextField(blank=True, null=True)
+	author = models.CharField(max_length=90, help_text="Use pen name, not real name")
+	review = models.TextField(blank=True, null=True)
 	date_reviewed = models.DateTimeField(blank=True, null=True)
-	is_favorite = models.BooleanField(default=False)
+	is_favorite = models.BooleanField(default=False, verbose_name="Favorite?")
+
+	def __str__(self):
+		return self.title
