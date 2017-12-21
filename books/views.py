a92c78b0cd28c91fe39a9d8efc2306f035fbeac6
Stout <stoutd@pool-1-151.staff.net.dc.usn.root.ent>
2017-12-21 16:37:24 -0500

    Add template with a few tags, change list_books function to render request for list template

--- books/views.py
+++ books/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,4 +6,4 @@ from django.shortcuts import render
 # takes request object, returns response object
 # This one returns the username of the logged in user
 def list_books(request):
-	return HttpResponse(request.user.username)
+	return render(request, "list.html")
