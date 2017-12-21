0e5e65ad63f0ae39d952d894e6a00612efae25d0
Stout <stoutd@pool-1-151.staff.net.dc.usn.root.ent>
2017-12-21 16:24:17 -0500

    Add home URL and test username response for home view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+# Functional view, defined as function
+# takes request object, returns response object
+# This one returns the username of the logged in user
+def list_books(request):
+	return HttpResponse(request.user.username)
